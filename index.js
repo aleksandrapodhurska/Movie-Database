c80dd96af9ae56d2798ed832e447dbaf672a56df
Aleks <chesneishaya93@gmail.com>
2021-01-20 17:23:18 +0200

    solution

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 "use strict";
 
 const numberOfFilms = +prompt('How many movies have you watched?', '');
-const movieName = prompt('What was the last film you\'ve watched?', '');
-const movieMark = +('Rate the movie from 1 to 10', '');
-const movie2Name = prompt('What was another film you\'ve watched recently?', ''); 
-const movie2Mark = +('Rate the movie from 1 to 10', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        'movieName': 'movieMark'
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt('What was the last film you\'ve watched?', ''),
+    b = +prompt('Rate the movie from 1 to 10', ''),
+    c = prompt('What was another film you\'ve watched recently?', ''),
+    d= +prompt('Rate the movie from 1 to 10', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
